81dc526c8749369b9ac0862f92c32b58847ac3cb
yannick_muenz <yannick.muenz@senacor.com>
2024-03-13 11:14:52 +0100

    Use angular environments to pass backendUrl

--- src/app/chatbot/services/openai.service.ts
+++ src/app/chatbot/services/openai.service.ts
@@ -3,30 +3,31 @@ import { Message } from '../models/message';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { setAuthHeaders } from '../../../utils/auth/auth-headers';
+import { environment } from '../../../environments/environment';
+
+console.log(environment.backendOrigin);
 
 @Injectable({
   providedIn: 'root'
 })
 export class OpenaiService {
 
-  //TODO: Dynamically load the backendUrl instead of hardcoding it
-  readonly backendURL = "https://chatbot-backend-csf37hag2a-ey.a.run.app" || "http://localhost:3000";
 
   constructor(private readonly httpClient: HttpClient) { }
   
   sendMessage(message:Message):Observable<Message> {
-    const serviceRequestConfig = setAuthHeaders(this.backendURL, "POST", message);
+    //const serviceRequestConfig = setAuthHeaders(environment.backendOrigin, "POST", message);
     return this.httpClient.post<Message>(
-      `${this.backendURL}/sendNewPrompt`, {
+      `${environment.backendOrigin}/sendNewPrompt`, {
         message,
       }
     );
   }
 
   initializeBot():Observable<Message> {
-    const serviceRequestConfig = setAuthHeaders(this.backendURL, "GET");
+    //const serviceRequestConfig = setAuthHeaders(environment.backendOrigin, "GET");
     return this.httpClient.get<Message>(
-      `${this.backendURL}/initBot`
+      `${environment.backendOrigin}/initBot`
     )
   }
 }
